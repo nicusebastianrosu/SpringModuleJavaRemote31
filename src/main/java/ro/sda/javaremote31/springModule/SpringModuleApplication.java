a730eb696553c6b3a9a8a66be462104a69d93a87
Nicu Rosu <nicu.rosu1967@gmail.com>
2022-03-24 12:51:58 +0200

    debugging front-end

---
+++ src/main/java/ro/sda/javaremote31/springModule/SpringModuleApplication.java
@@ -0,0 +1,14 @@
+package ro.sda.javaremote31.springModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringModuleApplication.class, args);
+	}
+
+}
